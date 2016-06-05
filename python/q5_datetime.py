9d704b31c45d2dafb106b65d6bb930613b914ac5
justindchien <justindchien@users.noreply.github.com>
2016-06-04 22:05:09 -0700

    coded the date differences

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,31 @@
 # Hint:  use Google to find python function
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+from datetime import datetime
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+####a)
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+start = datetime.strptime(date_start, '%m-%d-%Y')
+stop = datetime.strptime(date_stop, '%m-%d-%Y')
+diff = stop - start
+print(diff.days)
+
+####b)
+date_start = '12312013'
+date_stop = '05282015'
+
+start = datetime.strptime(date_start, '%m%d%Y')
+stop = datetime.strptime(date_stop, '%m%d%Y')
+diff = stop - start
+print(diff.days)
+
+
+####c)
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+
+start = datetime.strptime(date_start, '%d-%b-%Y')
+stop = datetime.strptime(date_stop, '%d-%b-%Y')
+diff = stop-start
+print (diff.days)
