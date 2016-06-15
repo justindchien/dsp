64a31f6242b832006dca5eee72110085d965d6ee
justindchien <justindchien@users.noreply.github.com>
2016-06-15 01:19:11 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,40 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+from pandas import Series
+faculty = pd.read_csv("faculty.csv")
+faculty[' degree'] = faculty[' degree'].str.replace('.','').str.strip()
+faculty[' title'] = faculty[' title'].str.replace('is ','of ')
+
+#Splitting first and last names
+faculty['first name'] = faculty.name.str.split('\s+').str[0]
+faculty['last name'] = faculty.name.str.split('\s+').str[-1]
+
+#Create first dictionary
+last_names = faculty['last name'].tolist()
+first_names = faculty['first name'].tolist()
+degrees = faculty[' degree'].tolist()
+title = faculty[' title'].tolist()
+email = faculty[' email'].tolist()
+
+data = [list(a) for a in zip(last_names, degrees, title, email)]
+
+faculty_dict = {}
+for n in range(len(data)):
+    if data[n][0] in faculty_dict.keys():
+        faculty_dict[data[n][0]].append(data[n][1:])
+    else:
+        faculty_dict[data[n][0]] = data[n][1:]
+
+#Create second dictionary
+full_names = zip(first_names, last_names)
+info = [list(b) for b in zip(degrees, title, email)]
+professor_dict = dict(zip(full_names, info))
+
+for key, value in sorted(professor_dict.items()):
+    print(key, value)
+    
+#Create keys with last name first
+last_first = zip(last_names, first_names)
+professor_dict2 = dict(zip(last_first, info))
+
+for k, val in sorted(professor_dict2.items()):
+    print(k, val)
